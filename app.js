126d95e17b9a49f42f8e0348a4e490597f277909
Kevin Erickson <kevin.erick01@gmail.com>
2017-09-03 20:52:36 -0600

    configure for cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 const routes = require('./routes');
@@ -9,6 +10,13 @@ const routes = require('./routes');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "credentials":"true",
+  "preflightContinue": true,
+  "optionsSuccessStatus": 204
+}));
 
 app.use('/', routes);
 
