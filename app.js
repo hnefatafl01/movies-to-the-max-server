e9e5cc1e07c1d18fee860ca6b49b9c939f8ab439
Kevin Erickson <kevin.erick01@gmail.com>
2017-09-04 09:50:11 -0600

    update cors origin url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const corsOptions = {
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
   credentials: true,
   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  origin: 'http://localhost:4200'
+  origin: 'https://movies-to-the-max.firebaseapp.com/'
 };
 
 const routes = require('./routes');
